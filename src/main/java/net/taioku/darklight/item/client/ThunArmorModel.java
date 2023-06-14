86b620f8846b65ba094ca5b0c2c40219b70d088b
taioku <t.rgpns@gnail.com>
2023-06-14 20:02:32 -0300

    0.0.1 - Testing Phase

--- src/main/java/net/taioku/darklight/item/client/ThunArmorModel.java
+++ src/main/java/net/taioku/darklight/item/client/ThunArmorModel.java
@@ -1,22 +1,23 @@
 package net.taioku.darklight.item.client;
 
 import net.minecraft.util.Identifier;
+import net.taioku.darklight.Darklight;
 import net.taioku.darklight.item.custom.ThunArmorItem;
 import software.bernie.geckolib.model.GeoModel;
 
 public class ThunArmorModel extends GeoModel<ThunArmorItem> {
     @Override
     public Identifier getModelResource(ThunArmorItem animatable) {
-        return null;
+        return new Identifier(Darklight.MOD_ID, "geo/thun_armor.geo.json");
     }
 
     @Override
     public Identifier getTextureResource(ThunArmorItem animatable) {
-        return null;
+        return new Identifier(Darklight.MOD_ID, "textures/armor/thun_armor.png");
     }
 
     @Override
     public Identifier getAnimationResource(ThunArmorItem animatable) {
-        return null;
+        return new Identifier(Darklight.MOD_ID, "animations/thun_armor.animations.json");
     }
 }
